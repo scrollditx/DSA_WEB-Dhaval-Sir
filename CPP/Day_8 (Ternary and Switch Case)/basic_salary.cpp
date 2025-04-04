7a7a07081705525beac01b4f6f614f5c24ab0c6d
scrollditx <parekhveddharmeshbhai@gmail.com>
2025-04-04 11:27:27 +0530

    changes done

--- CPP/Day_8 (Ternary and Switch Case)/basic_salary.cpp
+++ CPP/Day_8 (Ternary and Switch Case)/basic_salary.cpp
@@ -5,7 +5,7 @@ int main()
 {
     string e_name, e_id, e_dept, e_designation;
 
-    double basic_salary, hra, da, ta, i_tax, ma, pt, pf, total_allowances, total_expences, gross, net_salary;
+    double basic_salary, hra = 800, da, ta, i_tax, ma = 2000, pt = 200, pf, total_allowances, total_expences, gross, net_salary;
 
     cout << endl;
     cout << "Enter your Name: ";
@@ -23,18 +23,17 @@ int main()
     cout << "Enter your Basic Salary: ";
     cin >> basic_salary;
 
-    hra = 800;
     da = 0.15 * basic_salary;
     ta = 0.10 * basic_salary;
-    ma = 2000;
     total_allowances = hra + da + ta + ma;
     i_tax = 0.15 * basic_salary;
-    pt = 200;
     pf = 0.07 * basic_salary;
     total_expences = i_tax - pt - pf;
     gross = total_allowances - total_expences;
     net_salary = basic_salary + gross;
 
+    cout << " --------------------------- " << endl;
+    cout << "Employee's Salary Slip:                 " << endl;
     cout << " --------------------------- " << endl;
     cout << "Employee's ID:                 " << e_id << endl;
     cout << "Employee's Name:               " << e_name << endl;
